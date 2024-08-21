eec625407083ea3566171300bf6fb1447d791197
Oliver Bakic <oliver.bakic@hotmail.com>
2024-08-21 12:02:21 +0200

    removing elements solved

---
+++ removing_elements.py
@@ -0,0 +1,5 @@
+def remove_every_other(my_list):
+    return my_list[::2]
+
+def remove_every_third(my_list):
+    return my_list[::3]
--- testing/kata_test.py
+++ testing/kata_test.py
@@ -1,6 +1,7 @@
 from dailykatas import (
     do_i_get_a_bonus, 
-    count_the_monkeys
+    count_the_monkeys,
+    removing_elements
 )
 
 def test_monkey_count():
@@ -11,3 +12,13 @@ def test_do_i_get_a_bonus():
     assert do_i_get_a_bonus.bonus_time(5, True) == '$50'
     assert do_i_get_a_bonus.bonus_time(10, True) == '$100'
     assert do_i_get_a_bonus.bonus_time(10, False) == '$10'
+    
+def test_remove_every_other():
+    assert removing_elements.remove_every_other([1, 2, 3, 4, 5]) == [1, 3, 5]
+    assert removing_elements.remove_every_other(['a', 'b', 'c', 'd', 'e']) == ['a', 'c', 'e']
+    assert removing_elements.remove_every_other([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+    assert removing_elements.remove_every_other(["Keep", "Remove", "Keep", "Remove", "Keep"]) == ["Keep", "Keep", "Keep"]
+
+def test_remove_every_third():
+    assert removing_elements.remove_every_third([1, 2, 3, 4, 5, 6]) == [1, 4]
+    assert removing_elements.remove_every_third(['a', 'b', 'c', 'd', 'e', 'f']) == ['a', 'd']    
