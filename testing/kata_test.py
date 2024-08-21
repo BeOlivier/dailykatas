4df07ce6d1b0a2f9f7d23f679a553a8ffa672fdb
Oliver Bakic <oliver.bakic@hotmail.com>
2024-08-21 12:14:57 +0200

    fixed project folder structure to keep cleaner

--- testing/kata_test.py
+++ testing/kata_test.py
@@ -1,11 +1,10 @@
-from dailykatas import (
-    do_i_get_a_bonus, 
-    count_the_monkeys,
+from dailykatas.katas import (
+    do_i_get_a_bonus,
     removing_elements
 )
 
-def test_monkey_count():
-    assert count_the_monkeys.monkey_count(5) == [i for i in range(1, 6)]
+
+
     
 def test_do_i_get_a_bonus():
     assert do_i_get_a_bonus.bonus_time(2, True) == '$20'
